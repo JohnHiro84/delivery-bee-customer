abee4c59b36e7213d3f5239a0ae4f80daafee75a
Alexander Yambao <32828383+JohnHiro84@users.noreply.github.com>
2020-08-22 00:46:46 -0700

    Update review_api_util.js

--- src/util/review_api_util.js
+++ src/util/review_api_util.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 
 export const getSingleOrdersReview = id => {
-  return axios.get(`/api/orders/${id}/reviews`)
+  return axios.get(`https://immense-ridge-65848.herokuapp.com/api/orders/${id}/reviews`)
 };
 
 ////////////////// create review
 
 export const writeReview = data => {
-  return axios.post('/api/reviews/', data)
+  return axios.post('https://immense-ridge-65848.herokuapp.com/api/reviews/', data)
 }
